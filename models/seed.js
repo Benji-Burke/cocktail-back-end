b9c7881dc307b587946621115c6710713531d267
Laura Seely <llseely626@gmail.com>
2019-08-21 14:42:12 -0400

    wrote seed data

---
+++ models/seed.js
@@ -0,0 +1,51 @@
+let seedCocktails = [
+    {
+        name: 'margarita', 
+        alcoholic: 'alcoholic',
+        glass: 'cocktail glass',
+        ingredients: ['Tequila', 'Triple sec', 'Lime juice', 'salt'],
+        instructions: 'Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.'
+    },
+    {
+        name: 'manhattan',
+        alcoholic: 'alcoholic',
+        glass: 'cocktail glass',
+        ingredients: ['Sweet Vermouth', 'Bourbon', 'Angostura bitters', 'Ice','Maraschino cherry', 'Orange peel'],
+        instructions: 'Stirred over ice, strained into a chilled glass, garnished, and served up.'
+    },
+    {
+        name: 'gin and tonic',
+        alcoholic: 'alcoholic',
+        glass: 'Highball glass',
+        ingredients: ['Gin', 'Tonic water', 'Lime'],
+        instructions: 'Pour the gin and the tonic water into a highball glass almost filled with ice cubes. Stir well. Garnish with the lime wedge.'
+    },
+    {
+        name: 'mojito',
+        alcoholic: 'alcoholic',
+        glass: 'Highball glass',
+        ingredients: ['Light rum', 'Lime', 'Sugar', 'Mint', 'Soda water'],
+        instructions: 'Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish and serve with straw.'
+    },
+    {
+        name: 'martini',
+        alcoholic: 'alcoholic',
+        glass: 'Cocktail glass',
+        ingredients: ['Gin', 'Dry Vermouth', 'Olive'],
+        instructons: 'Straight: Pour all ingredients into mixing glass with ice cubes. Stir well. Strain in chilled martini cocktail glass. Squeeze oil from lemon peel onto the drink, or garnish with olive.'
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = seedCocktails
