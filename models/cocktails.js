cb5784e16dc5dbc6982925af9ee2601390266a44
Ben Burke <benjaminstevenburke@gmail.com>
2019-08-22 16:18:04 -0400

    added favorite property

--- models/cocktails.js
+++ models/cocktails.js
@@ -5,7 +5,8 @@ const cocktailSchema = mongoose.Schema({
     alcoholic: String,
     glass: String,
     instructions: String,
-    ingredients: Array
+    ingredients: Array,
+    favorite: Boolean, default: false
 
 })
 
